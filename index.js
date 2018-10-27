3c55f356aeaa95a74f222b10a30a196cc9506ed6
David Floyd <davidfloyd91@gmail.com>
2018-10-27 19:22:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function prependKitten(name) {
   return name.concat(kittens);
 }
 function removeLastKitten() {
-  return kittens.slice(1, kittens.length);
+  return kittens.slice(0, kittens.length);
 }
