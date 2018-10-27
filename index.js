3b94bc41ca06cb6d90a6a91acfb2e7af97358930
David Floyd <davidfloyd91@gmail.com>
2018-10-27 19:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function destructivelyRemoveFirstKitten(name) {
   kittens.shift(name);
 }
 function appendKitten(name) {
-  var kittens = [...kittens, name]
+  var newKittens = [...kittens, name];
 }
