9867dd926aa329b21c54709892ef86468696c8c1
David Floyd <davidfloyd91@gmail.com>
2018-10-27 19:06:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var kittens = ['Milo', 'Otis', 'Garfield']; //define your array here
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
-
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
 // Add your functions and code here
